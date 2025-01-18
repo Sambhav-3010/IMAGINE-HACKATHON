b33a8ff3e3972c34407e3c83697794f368bf947e
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2025-01-18 16:36:57 +0530

    hero completed

--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -5,16 +5,16 @@ import "./hero.css";
 const Hero: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-br from-black via-gray-800 to-black text-white">
-      <div className="flex flex-col items-center justify-center rounded-lg text-white bg-white/10 backdrop-blur-md shadow-lg">
-        <div className="hero flex flex-col items-center justify-center rounded-lg bg-white/1 backdrop-blur-md p-6 shadow-md">
-          <h1 className="hero1 text-4xl font-bold mb-4 text-center drop-shadow-lg">
-            Open Source User Authentication
-            <p className="text-lg mb-8 text-center drop-shadow-lg"></p>
-            <Button className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
-              Click me
-            </Button>
-          </h1>
-        </div>
+      <div className="hero flex flex-col items-center justify-center rounded-lg bg-white/5 backdrop-blur-lg p-6 shadow-lg border border-white/30">
+      <h1 className="hero1 flex flex-col items-center justify-center text-4xl bg-white/10 font-bold mb-4 text-center drop-shadow-lg backdrop-blur-lg p-6 border-white/10 rounded-xl">
+        Open Source User Authentication
+        <p className="text-lg mb-8 text-center drop-shadow-lg">
+        Build fast. Maintain control. Reduce costs.
+        </p>
+        <Button className="px-6 py-2 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600">
+        Click me
+        </Button>
+      </h1>
       </div>
     </div>
   );
